3f1b6fce8e1a1720ac602570429b1f3990ece6d1
daniel1027-coder <jn30802@yahoo.com.tw>
2026-01-31 18:46:04 -0500

    Add files via upload

---
+++ Lab3_Poshun_list.py
@@ -0,0 +1,14 @@
+"""
+Working with a list
+Poshun Lin
+
+Making lists and modify them. This file is to create a camping list.
+No starter code was used. This program was written from scratch
+
+01/31/2026
+"""
+
+items = ["towel", "swiss army knife", "hat", "smartphone", "book", "water bottle", "snacks", "rubbing alcohol", "headphones", "sleeping bag"]
+items.sort()
+print(len(items))
+print(items)
